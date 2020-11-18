c1c7bb3a636210152028644fa0ef3f8c47dc8016
Artem Korolev <korart@gmail.com>
2020-11-18 09:42:19 +0300

    Box class example

---
+++ src/BoxDemo.java
@@ -0,0 +1,11 @@
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box box = new Box();
+        box.width = 10;
+        box.height = 15;
+        box.depth = 20;
+
+        double volume = box.width * box.height * box.depth;
+        System.out.println("Объем коробки: " + volume);
+    }
+}
